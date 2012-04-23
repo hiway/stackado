9ff07056d400782fe2f55361aca4f8c9b5a403b0
Harshad Sharma <harshad@harshadsharma.com>
2012-04-24 03:52:52 +0530

    Starting fresh for stackado, better API.

---
+++ stackado.py
@@ -0,0 +1,18 @@
+"""
+    Stackado
+================================================================================
+
+    Stack-a-do, a task management system that lets you get distracted or dig
+    deeper into the task at hand and come back to whatever you were doing.
+    From as many levels deep as you choose to go.
+
+    This module exposes an API that we can use to write command-line app,
+    chat-bots or even email or sms-based applications to manipulate our
+    todo-stack.
+
+"""
+
+# We'll be using json to save user's data into
+# files for persistent memory.
+import json
+
