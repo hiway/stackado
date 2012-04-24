89aff4efe258067f87978843c628fbbd14046af7
Harshad Sharma <harshad@harshadsharma.com>
2012-04-24 06:12:26 +0530

    Adding commandline interface.

---
+++ todo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# importing readling makes raw_input behave better
+# refer: http://docs.python.org/library/functions.html#raw_input
+import readline
+import stackado
+
+stack = stackado.TodoStack()
+filename = 'autosave.json'
+
+try:
+    # Try to load data from disk
+    data = open(filename, 'r+').read()
+    stack.load_state(data)
+except:
+    # Unable to load data, ignore
+    pass
+
+carry_on = True
+while carry_on is True:
+    line = raw_input("> ")
+    if line != 'q':
+        print stack.parse_command(line)
+
+        # Save state
+        open(filename, 'w+').write(stack.dump_state())
+
+    else:
+        carry_on = False
+
